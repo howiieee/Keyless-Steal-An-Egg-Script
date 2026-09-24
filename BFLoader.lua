efe605e8800f8205fd79123c49318560cd095a1b
howiieee <bluealpha1365@gmail.com>
2026-09-24 22:31:35 +0800

    Update BFLoader.lua

--- BFLoader.lua
+++ BFLoader.lua
@@ -145,7 +145,7 @@ local function showAuthUI()
 end
 
 local ROUTES = {
-    [107778070777162] = "https://raw.githubusercontent.com/howiieee/Keyless-Steal-An-Egg-Script/refs/heads/main/ObfSaeLoader.lua",
+    [107778070777162] = "https://raw.githubusercontent.com/howiieee/Keyless-Steal-An-Egg-Script/refs/heads/main/SaeLoader.lua",
 }
 
 local function loadScriptForPlace()
